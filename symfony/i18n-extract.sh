4a30e8015013a5c9f702414827715d879c221989
Baptiste Dulac <dulacbaptiste@gmail.com>
2023-12-01 12:50:17 +0800

    Add Taskfile.yaml and i18n-extract.sh files for Symfony project

---
+++ symfony/i18n-extract.sh
@@ -0,0 +1,31 @@
+# Usage: ./i18n-extract.sh
+ARG_1=$1
+BLUE='\033[0;34m'
+NC='\033[0m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+
+# Fail on error
+set -e
+
+echo -e "${BLUE}----------------------------------${NC}"
+echo -e "${BLUE}- \033[0;34mExtracting i18n\033[0m"
+echo -e "${BLUE}----------------------------------${NC}"
+
+# Arg1 is a list of locales to extract, comma separated (e.g. en,fr)
+IFS=',' read -ra LOCALES <<< "$ARG_1"
+
+# Make sure we have at least one locale
+if [ ${#LOCALES[@]} -eq 0 ]; then
+    echo -e "${RED}No locales specified${NC}"
+    exit 1
+fi
+
+# Extract i18n
+for LOCALE in "${LOCALES[@]}"; do
+    echo -e "${YELLOW}➡ Extracting i18n for locale ${GREEN}${LOCALE}${NC}"
+    symfony console t:e --force --domain messages --sort --format yaml $LOCALE -q
+done
+
+# Done (green)
+echo -e "${GREEN}✓ Done${NC}"
